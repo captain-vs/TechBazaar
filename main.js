5039481ac15f7b4cfad09c42f52bea856db8e996
Vishnu Suthar <sutharvishnu511@gmail.com>
2025-02-11 12:50:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,200 @@
+var swiper = new Swiper('.product-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    // initialSlide: 2,
+    loop: false,
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev'
+    },
+    // mousewheel: {
+    //     // invert: false
+    // },
+    on: {
+        init: function(){
+            var index = this.activeIndex;
+
+            var target = $('.product-slider__item').eq(index).data('target');
+
+            console.log(target);
+
+            $('.product-img__item').removeClass('active');
+            $('.product-img__item#'+ target).addClass('active');
+        }
+    }
+
+});
+
+swiper.on('slideChange', function () {
+    var index = this.activeIndex;
+
+    var target = $('.product-slider__item').eq(index).data('target');
+
+    console.log(target);
+
+    $('.product-img__item').removeClass('active');
+    $('.product-img__item#'+ target).addClass('active');
+
+    if(swiper.isEnd) {
+        $('.prev').removeClass('disabled');
+        $('.next').addClass('disabled');
+    } else {
+        $('.next').removeClass('disabled');
+    }
+
+    if(swiper.isBeginning) {
+        $('.prev').addClass('disabled');
+    } else {
+        $('.prev').removeClass('disabled');
+    }
+});
+
+$(".js-fav").on("click", function() {
+    $(this).find('.heart').toggleClass("is-active");
+});
+
+const colors = ["#FF5733", "#007BFF", "#32CD32", "#6A0DAD", "#FFD700"];
+let currentIndex = 0;
+
+function changeBackgroundColor() {
+    const topDeals = document.getElementById("deal");
+    topDeals.style.backgroundColor = colors[currentIndex];
+    currentIndex = (currentIndex + 1) % colors.length;
+}
+
+setInterval(changeBackgroundColor, 3000); // Change color every 2 seconds
+
+const dealsContainer = document.querySelector('.deals-container');
+const deals = document.querySelectorAll('.deales');
+
+let currentIndex1 = 0;
+
+function showNextDeal() {
+  deals[currentIndex1].classList.remove('active');
+  currentIndex1 = (currentIndex1 + 1) % deals.length;
+  deals[currentIndex1].classList.add('active');
+}
+
+function showPrevDeal() {
+  deals[currentIndex1].classList.remove('active');
+  currentIndex1 = (currentIndex1 - 1 + deals.length) % deals.length;
+  deals[currentIndex1].classList.add('active');
+}
+
+setInterval(showNextDeal, 5000);
+
+// Add event listeners to the buttons
+deals.forEach((deal) => {
+  deal.querySelector('button').addEventListener('click', () => {
+    // Handle the "Buy Now" button click event
+    console.log(`Deal ${deal.querySelector('h3').textContent} clicked!`);
+  });
+});
+
+
+const addToCart = (product_id) => {
+    let positionThisProductInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(cart.length <= 0){
+        cart = [{
+            product_id: product_id,
+            quantity: 1
+        }];
+    }else if(positionThisProductInCart < 0){
+        cart.push({
+            product_id: product_id,
+            quantity: 1
+        });
+    }else{
+        cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+const addCartToMemory = () => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    if(cart.length > 0){
+        cart.forEach(item => {
+            totalQuantity = totalQuantity +  item.quantity;
+            let newItem = document.createElement('div');
+            newItem.classList.add('item');
+            newItem.dataset.id = item.product_id;
+
+            let positionProduct = products.findIndex((value) => value.id == item.product_id);
+            let info = products[positionProduct];
+            listCartHTML.appendChild(newItem);
+            newItem.innerHTML = `
+            <div class="image">
+                    <img src="${info.image}">
+                </div>
+                <div class="name">
+                ${info.name}
+                </div>
+                <div class="totalPrice">$${info.price * item.quantity}</div>
+                <div class="quantity">
+                    <span class="minus"><</span>
+                    <span>${item.quantity}</span>
+                    <span class="plus">></span>
+                </div>
+            `;
+        })
+    }
+    iconCartSpan.innerText = totalQuantity;
+}
+
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
+        let product_id = positionClick.parentElement.parentElement.dataset.id;
+        let type = 'minus';
+        if(positionClick.classList.contains('plus')){
+            type = 'plus';
+        }
+        changeQuantityCart(product_id, type);
+    }
+})
+const changeQuantityCart = (product_id, type) => {
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(positionItemInCart >= 0){
+        let info = cart[positionItemInCart];
+        switch (type) {
+            case 'plus':
+                cart[positionItemInCart].quantity = cart[positionItemInCart].quantity + 1;
+                break;
+        
+            default:
+                let changeQuantity = cart[positionItemInCart].quantity - 1;
+                if (changeQuantity > 0) {
+                    cart[positionItemInCart].quantity = changeQuantity;
+                }else{
+                    cart.splice(positionItemInCart, 1);
+                }
+                break;
+        }
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+
+const initApp = () => {
+    // get data product
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+
+        // get data cart from memory
+        if(localStorage.getItem('cart')){
+            cart = JSON.parse(localStorage.getItem('cart'));
+            addCartToHTML();
+        }
+    })
+}
+initApp();
+
+
+ 
